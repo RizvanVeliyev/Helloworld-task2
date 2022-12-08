32e3ad25ddb70a75baa3c427b44f2473b7b92cbd
RizvanVeliyev <veliyevrizvan250@gmail.com>
2022-12-08 09:44:49 +0400

    Helloworld-task2

---
+++ hw/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace hw
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("a:");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.Write("b:");
+			int b = Convert.ToInt32(Console.ReadLine());
+			int balance;
+			balance = a % b;
+			Console.Write("qaliq:");
+			Console.WriteLine(balance);
+
+		}
+	}
+}
